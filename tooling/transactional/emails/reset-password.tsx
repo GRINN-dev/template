3c436984adfba338c6a600bf56561de8bbecfa70
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2024-11-08 14:38:14 +0100

    template mail reset password

---
+++ tooling/transactional/emails/reset-password.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import {
+  Body,
+  Button,
+  Column,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+
+interface ResetPasswordEmailProps {
+  username: string;
+  url: string;
+  token: string;
+}
+
+export const ResetPasswordEmail = ({
+  username,
+  url,
+  token,
+}: ResetPasswordEmailProps) => {
+  return (
+    <Html>
+      <Head />
+      <Body>
+        <Tailwind>
+          <Container>
+            <Section>
+              <Row>
+                <Column>
+                  <Heading>Réinitialisation de mot de passe</Heading>
+                  <Text>Bonjour {username},</Text>
+                  <Text>
+                    Vous avez demandé à réinitialiser votre mot de passe. Si
+                    vous n'avez pas demandé de réinitialisation de mot de passe,
+                    vous pouvez ignorer cet e-mail.
+                  </Text>
+                  <Text>
+                    Pour réinitialiser votre mot de passe, cliquez sur le bouton
+                    ci-dessous :
+                  </Text>
+                  <Button
+                    href={`${url}?token=${token}`}
+                    className="rounded-full bg-black px-4 py-2 text-white"
+                  >
+                    Réinitialiser mon mot de passe
+                  </Button>
+                </Column>
+              </Row>
+            </Section>
+          </Container>
+        </Tailwind>
+      </Body>
+    </Html>
+  );
+};
+
+export default ResetPasswordEmail;
