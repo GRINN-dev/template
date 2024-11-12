a987afcce693a6175463e080e52fd6fdca82d749
NinonMaraval <79524843+NinonMaraval@users.noreply.github.com>
2024-11-12 11:14:46 +0100

    quick style

--- apps/mobile/src/app/(auth)/login/index.tsx
+++ apps/mobile/src/app/(auth)/login/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { Button, SafeAreaView, Text, TextInput } from "react-native";
-import { Link } from "expo-router";
+import {
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from "react-native";
+import { Link, router } from "expo-router";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
 import z from "zod";
@@ -19,71 +26,78 @@ const Login = () => {
   });
 
   return (
-    <SafeAreaView>
-      <Text className="text-xl">Connectez-vous</Text>
-      <Controller
-        control={form.control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            onBlur={onBlur}
-            keyboardType="default"
-            onChangeText={(value) => onChange(value)}
-            value={value}
-            placeholder="Pseudonyme"
-            autoCapitalize="none"
-          />
+    <ScrollView contentContainerStyle={styles.container}>
+      <SafeAreaView>
+        <Text className="text-xl">Connectez-vous</Text>
+        <Controller
+          control={form.control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              onBlur={onBlur}
+              keyboardType="default"
+              onChangeText={(value) => onChange(value)}
+              value={value}
+              placeholder="Pseudonyme"
+              autoCapitalize="none"
+              style={styles.input}
+            />
+          )}
+          name="username"
+          rules={{ required: true }}
+        />
+        {form.formState.errors.username && (
+          <Text>{form.formState.errors.username.message! as string}</Text>
         )}
-        name="username"
-        rules={{ required: true }}
-      />
-      {form.formState.errors.username && (
-        <Text>{form.formState.errors.username.message! as string}</Text>
-      )}
 
-      {/* <TextInput style={styles.input} secureTextEntry {...form.register('password')} /> */}
-      <Controller
-        control={form.control}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            onBlur={onBlur}
-            onChangeText={(value) => onChange(value)}
-            value={value}
-            // secureTextEntry={!isPasswordVisible}
-            placeholder="Mot de passe"
-            keyboardType="visible-password"
-            autoCapitalize="none"
-          />
-        )}
-        name="password"
-      />
+        {/* <TextInput style={styles.input} secureTextEntry {...form.register('password')} /> */}
+        <Controller
+          control={form.control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+              // secureTextEntry={!isPasswordVisible}
+              placeholder="Mot de passe"
+              keyboardType="visible-password"
+              autoCapitalize="none"
+              style={styles.input}
+            />
+          )}
+          name="password"
+        />
 
-      <Button
-        title="Login"
-        onPress={async () => {
-          await client
-            .mutate({
-              mutation: loginMutation,
-              variables: {
-                username: form.getValues("username"),
-                password: form.getValues("password"),
-              },
-            })
-            .catch((error) => {
-              console.error(error);
-            })
-            .then(() => {
-              console.log("Login success");
-            });
-        }}
-      />
+        <Pressable
+          style={styles.button}
+          onPress={async () => {
+            await client
+              .mutate({
+                mutation: loginMutation,
+                variables: {
+                  username: form.getValues("username"),
+                  password: form.getValues("password"),
+                },
+              })
+              .catch((error) => {
+                console.error(error);
+              })
+              .then(() => {
+                console.log("Login success");
+                router.push("/(home)");
+              });
+          }}
+        >
+          <Text style={styles.buttonText}>Login </Text>
+        </Pressable>
 
-      <Text>
-        Vous n&apos;avez pas de compte ?{" "}
-        <Link href={{ pathname: "/(auth)/register" }} className="underline">
-          Inscrivez-vous
-        </Link>
-      </Text>
-    </SafeAreaView>
+        <Text>
+          Vous n&apos;avez pas de compte ?{" "}
+          <Link href={{ pathname: "/(auth)/register" }} className="underline">
+            Inscrivez-vous
+          </Link>
+        </Text>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
@@ -101,3 +115,39 @@ const loginMutation = graphql(`
     }
   }
 `);
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    justifyContent: "space-between",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    height: "100%",
+    paddingVertical: 20,
+  },
+  input: {
+    marginTop: 20,
+    height: 64,
+    borderColor: "#CDCCC9",
+    borderWidth: 1,
+    borderRadius: 6,
+    backgroundColor: "white",
+    padding: 10,
+    color: "black",
+  },
+  button: {
+    marginVertical: 20,
+    height: 64,
+    borderRadius: 6,
+    backgroundColor: "white",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "black",
+    fontSize: 16,
+  },
+});
---
+++ apps/mobile/src/app/(auth)/register/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Register = () => {
+  return (
+    <View>
+      <Text>Inscription</Text>
+    </View>
+  );
+};
+
+export default Register;
